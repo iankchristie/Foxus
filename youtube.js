a6c904c19d460bb8660674e7d074ed058411e194
Ian Christie <iankchristie@gmail.com>
2017-08-26 16:46:26 -0700

    df

--- youtube.js
+++ youtube.js
@@ -2,12 +2,12 @@
 // This scanning functionality allows the user to hover over an 
 // image and see the GIF summary.
 function removeImages() {
-	var images = document.getElementsByTagName('img');
-	for (var i = 0; i < images.length; i++) {
-  	 	images[i].src = '';
-    }
+  var images = document.getElementsByTagName('img');
+  for (var i = 0; i < images.length; i++) {
+    images[i].src = '';
+  }
 
-  	setTimeout(removeImages, 5000);
+  setTimeout(removeImages, 5000);
 }
 
 removeImages();
