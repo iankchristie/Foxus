403190a3a496465e6acd35ef902f01c45207e66a
Ian Christie <iankchristie@gmail.com>
2017-08-26 17:26:43 -0700

    deleting files

--- eventPage.js
+++
@@ -1,6 +0,0 @@
-// Navigate away from the default Facebook home feed to the users profile.
-chrome.webNavigation.onBeforeNavigate.addListener(function(details) {
-	if (details.url == 'https://www.facebook.com/') {
-		chrome.tabs.update(details.tabId, { url: "https://www.facebook.com/profile" });	
-	}
-});
--- facebook.js
+++
@@ -1,3 +0,0 @@
-// Disable Home Button
-var homeButton = document.getElementById("u_0_c");
-homeButton.href = "";
